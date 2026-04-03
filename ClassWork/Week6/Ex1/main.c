ac91312c7790282e75ca0e04fcace63e621257b9
xhovan-7 <xhovanisalepi@gmail.com>
2026-04-03 11:25:58 +0200

    Enhance student data processing and sorting
    Refactor student grade handling and improve input validation.

--- ClassWork/Week6/Ex1/main.c
+++ ClassWork/Week6/Ex1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "func.h"
+#include <string.h>
+
 
 int getSize(){
     int size;
@@ -11,10 +12,20 @@ int getSize(){
         printf("Enter a number between 1 - 100: ");
         scanf("%d", &size);
     }
-
     return size;
 }
 
+double getTarget(){
+    double target;
+    printf("Enter the grade to search for: ");
+    scanf("%lf", &target);
+
+    while(target <= 0 && target > 100){
+        printf("Enter a grade between 1 - 100: ");
+        scanf("%d", &target);
+    }
+    return target;
+}
 
 typedef struct Student{
     char name[16];
@@ -24,7 +35,6 @@ typedef struct Student{
 
 void getNameGrades(Student students[], int size){
 
-    
     for (int i = 0; i < size; i++){
         printf("%d: ", i);
         scanf("%s", students[i].name);
@@ -33,31 +43,113 @@ void getNameGrades(Student students[], int size){
         while(1){
             scanf("%d", &students[i].grades[j]);
 
-            if (students[i].grades[j++] == -999)   break;
+            if (students[i].grades[j] == -999){
+                students[i].grades[j] = 0;
+                break;
+            }
+            j++;
         }
 
-        int sum = 0;
+
+        double sum = 0;
         for (int k = 0; k < j; k++){
             sum += students[i].grades[k];
         }
+        students[i].avg = sum / j;
+        }
+}
 
-        students[i].avg = (float)sum / (float)j;
+void sortByAvg(Student students[], int size){
+
+    for (int i = 0; i < size - 1; i++){
+        int min = i;
+
+        for (int j = i + 1; j < size; j++){
+            if (students[min].avg > students[j].avg){
+                min = j;
+            }
+        }
+        int k = 0;
+
+        Student temp;
+        temp= students[i];
+        students[i] = students[min];
+        students[min] = temp;
+
+
+ /*       strcpy(temp.name, students[i].name);
+        temp.avg = students[i].avg;
+        do{
+            temp.grades[k] = students[i].grades[k];
+        }while (students[i].grades[k++] != 0);*/
+
 
     }
 }
 
+void printStudent(Student students[], int size){
+    for (int i = 0; i < size; i++){
+        printf("%d: %s- avg: %.2lf, Grades: ", i + 1, students[i].name, students[i].avg);
+        for (int j = 0; ;j++){
+            if (students[i].grades[j] == 0){
+                printf(". \n");
+                break;
+            }
 
+            printf("%d, ", students[i].grades[j]);
+        }
+    }
+}
 
+int binarySearch(Student students[], int size, double target){
+    int left = 0;
+    int right = size - 1;
+    int result = -1;
+
+    while(left <= right){
+        int mid = (left + right) / 2;
+
+        if (students[mid].avg >= target){
+            right = mid - 1;
+            result = mid;
+        }
+
+        else if (students[mid].avg < target){
+            left = mid + 1;
+        }
+    }
+
+    return result;
+}
+
+void printTarget(Student students[], int size, int index, double target){
+    printf("Student is at position %d\n", index + 1);
+
+    printf("Students with average grater than or equal to %.2lf\n", target);
+
+    for (int i = index, j = 0; i < size; i++, j++){
+        printf("%d: %s - %.2lf\n", i + 1, students[i].name, students[i].avg);
+    }
+
+}
 
 int main(){
     int size;
-    size = getSize;
+    size = getSize();
 
     Student students[100];
 
     getNameGrades(students, size);
 
+    sortByAvg(students, size);
+
+    double target = getTarget();
+
+    int targetIndex = binarySearch(students, size, target);
+
+    printTarget(students, size, targetIndex, target);
 
 
-    return 0; 
+
+    return 0;
 }

1211f5a510b069cd080257fab08518f91beb1631
xhovan-7 <xhovanisalepi@gmail.com>
2026-05-08 11:21:55 +0200

    .

--- ClassWork/Week11/11_8.c
+++ ClassWork/Week11/11_8.c
@@ -3,9 +3,10 @@
 
 void uppercase(char *p){
 
-    for(; *p != '\0';p++){
-        if (*p == ' ') continue;
+    while(*p != '\0'){
+        if (*p == ' '){ p++;    continue;   }
         *p -= 32;
+        p++;
     }
 
 
@@ -13,12 +14,15 @@ void uppercase(char *p){
 
 int main(){
     char name[20];
+
     printf("Enter the word in lowercase:");
     scanf("%19[^\n]", name);
+    
     char *p = name;
+    
     uppercase(p);
+    
     printf("%s", name);
-
     
     return 0;
 }
